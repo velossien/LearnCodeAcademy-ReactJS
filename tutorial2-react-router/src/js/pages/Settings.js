4b71c0e3e153ad3d7aee3fd425ec8f07caa2b60c
Tara Grimm <tararochegrimm@gmail.com>
2017-11-02 14:18:34 -0400

    Tutorial 2 - React-router (old -v1)

---
+++ tutorial2-react-router/src/js/pages/Settings.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Settings extends React.Component {
+  render() {
+    console.log("settings");
+    return (
+      <div>
+        <h1>Settings</h1>
+        <p> You can't change anything. lul.</p>
+      </div>
+    );
+  }
+}
